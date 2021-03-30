cb3e8c7d3bb739a60f6dddb3e6bf7c4572f32a2b
Jeff <wodud6359@gmail.com>
2021-03-30 23:06:12 +1300

    no message

---
+++ Lab01Program2.py
@@ -0,0 +1,24 @@
+"""
+Jaeyoung Hwang (Jeff), 12.03.2020
+
+Compleate the program so that it calculates
+the monthly repayments that need to be made to pay off a house loan.
+"""
+
+
+
+
+
+loan_amount = 650000 
+annual_interest_rate = 5
+loan_length_years = 20
+month_per_year = 12
+
+monthly_interest_rate = annual_interest_rate / month_per_year
+total_period = loan_length_years * month_per_year
+
+number_of_payments = loan_amount * (((monthly_interest_rate / 100) * (1 + monthly_interest_rate / 100)
+                                    ** total_period) / ((1+monthly_interest_rate/100)** total_period -1))
+
+
+print("You will need to pay $", round(number_of_payments), " monthly", sep="")
